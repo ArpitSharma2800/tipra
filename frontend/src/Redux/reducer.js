7b1dd9de8d562471b56714caf3de6aa02e60b033
ArpitSharma2800 <1arpit2000sharma@gmal.com>
2021-07-03 16:13:12 +0530

    only server

--- frontend/src/Redux/reducer.js
+++
@@ -1,9 +0,0 @@
-import {
-  combineReducers
-} from 'redux'
-import dashboardReducer from './dashbard/dashbardReducer'
-const Root = combineReducers({
-  dashboard: dashboardReducer
-})
-
-export default Root
